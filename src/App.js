eb660b9319cd0be23a2df6a94f297083dd3c09be
Tina <tina.hm.gao@gmail.com>
2022-04-25 15:23:44 +1200

    😈optimized class component template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            monsters: [],
+            searchField: '',
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => {
+                this.setState(() => {
+                    return { monsters: users }
+                })
+            })
+    }
+
+    onSearchChange = (event) => {
+        const searchField = event.target.value.toLocaleLowerCase()
+
+        this.setState(() => {
+            return { searchField }
+        })
+    }
+
+    render() {
+        const { monsters, searchField } = this.state
+        const { onSearchChange } = this
+
+        const filteredMonsters = monsters.filter((monster) =>
+            monster.name.toLocaleLowerCase().includes(searchField)
+        )
+
+        return (
+            <div className="App">
+                <input
+                    className="search-box"
+                    type="search"
+                    placeholder="search monsters"
+                    onChange={onSearchChange}
+                />
+                {filteredMonsters.map((monster, index) => {
+                    return (
+                        <div key={index}>
+                            <h1>
+                                {index + 1}: {monster.name}
+                            </h1>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
